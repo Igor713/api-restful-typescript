566f19543ecdc8f9cb0869d67e553848cef151d4
Igor713 <igormatheussiquieri@gmail.com>
2025-02-06 08:30:22 -0300

    created router find by id

--- src/controllers/movieControllers.ts
+++ src/controllers/movieControllers.ts
@@ -5,12 +5,40 @@ import { MovieModel } from '../models/Movie'
 
 // Logger
 import Logger from '../../config/logger'
+import { error } from 'console'
+import mongoose from 'mongoose'
 
 export async function createMovie(req: Request, res: Response): Promise<void> {
   try {
     const data = req.body
     const movie = await MovieModel.create(data)
     res.status(201).json(movie)
+  } catch (error: any) {
+    Logger.error(error.message)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+}
+
+export async function findMovieById(
+  req: Request,
+  res: Response,
+): Promise<void> {
+  try {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      res.status(400).json({ error: 'ID inválido' })
+      return
+    }
+
+    const movie = await MovieModel.findById(id)
+
+    if (!movie) {
+      res.status(404).json({ error: 'O filme não existe' })
+      return
+    }
+
+    res.status(200).json(movie)
   } catch (error: any) {
     Logger.error(error.message)
     res.status(500).json({ error: 'Internal Server Error' })
--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,13 @@
 import { Router, Request, Response } from 'express'
-import { createMovie } from './controllers/movieControllers'
+import { createMovie, findMovieById } from './controllers/movieControllers'
+
+//validations
+import { validate } from './middleware/handleValidation'
+import { movieCreateValidation } from './middleware/movieValidation'
 
 const router = Router()
 
-// router.get('/test', (req: Request, res: Response) => {
-//   res.status(200).send('API working!')
-// })
-
-router.post('/movie', createMovie)
+router.post('/movie', movieCreateValidation(), validate, createMovie)
+router.get('/movie/:id', findMovieById)
 
 export default router
