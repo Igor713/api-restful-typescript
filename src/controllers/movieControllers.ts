6d62238f9d45f689fccde6bca2803dbcb553ce7b
Igor713 <igormatheussiquieri@gmail.com>
2025-02-10 07:30:40 -0300

    finished

--- src/controllers/movieControllers.ts
+++ src/controllers/movieControllers.ts
@@ -44,3 +44,69 @@ export async function findMovieById(
     res.status(500).json({ error: 'Internal Server Error' })
   }
 }
+
+export async function getAllMovies(req: Request, res: Response) {
+  try {
+    const movies = await MovieModel.find()
+    if (!movies) {
+      res.status(404).json({ error: 'Não existem filmes para serem buscados' })
+      return
+    }
+
+    res.status(200).json(movies)
+  } catch (error: any) {
+    Logger.error(error.message)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+}
+
+export async function removeMovie(req: Request, res: Response) {
+  try {
+    const id = req.params.id
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      res.status(400).json({ error: 'ID inválido' })
+      return
+    }
+
+    const movie = await MovieModel.findById(id)
+
+    if (!movie) {
+      res.status(404).json({ error: 'O filme não existe' })
+      return
+    }
+
+    await movie.deleteOne()
+
+    res.status(200).json({ message: 'Filme removido com sucesso' })
+  } catch (error: any) {
+    Logger.error(error.message)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+}
+
+export async function updateMovie(req: Request, res: Response) {
+  try {
+    const id = req.params.id
+    const data = req.body
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      res.status(400).json({ error: 'ID inválido' })
+      return
+    }
+
+    const movie = await MovieModel.findById(id)
+
+    if (!movie) {
+      res.status(404).json({ error: 'O filme não existe' })
+      return
+    }
+
+    await MovieModel.updateOne({ _id: id }, data)
+
+    res.status(200).json(data)
+  } catch (error: any) {
+    Logger.error(error.message)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+}
--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,11 @@
 import { Router, Request, Response } from 'express'
-import { createMovie, findMovieById } from './controllers/movieControllers'
+import {
+  createMovie,
+  findMovieById,
+  getAllMovies,
+  removeMovie,
+  updateMovie,
+} from './controllers/movieControllers'
 
 //validations
 import { validate } from './middleware/handleValidation'
@@ -9,5 +15,8 @@ const router = Router()
 
 router.post('/movie', movieCreateValidation(), validate, createMovie)
 router.get('/movie/:id', findMovieById)
+router.get('/movie', getAllMovies)
+router.delete('/movie', removeMovie)
+router.patch('/movie:id', movieCreateValidation(), validate, updateMovie)
 
 export default router
